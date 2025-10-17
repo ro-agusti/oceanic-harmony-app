67f2acefdb1d29a55655c6e2b1dc71ea5d06e4c8
roAgusti <rochoagusti@gmail.com>
2025-10-17 18:52:22 +1000

    db

--- config/database.js
+++ config/database.js
@@ -3,10 +3,15 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    logging: false, // Optional
+const db = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "mysql",
+  logging: false,
 });
+
+// const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//     host: process.env.DB_HOST,
+//     dialect: 'mysql',
+//     logging: false, // Optional
+// });
 
 export { db };
