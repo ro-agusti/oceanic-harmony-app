84dcdad1b85e1ac588a434e580eab1abfe35c518
roAgusti <rochoagusti@gmail.com>
2025-10-25 13:25:12 +1000

    fix: backend should use Render port instead of MySQL port

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import userResponsesRoutes from './routes/userResponsesRoutes.js';
 dotenv.config(); // Load environment variables
 
 const app = express();
-const PORT = process.env.PORT || 3006;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 // Middlewares
 app.use(cors());
